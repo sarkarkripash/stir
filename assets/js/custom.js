6809d55169370fee79838c837401cebfc3316ef9
Sarkar Kripash <82843308+sarkarkripash@users.noreply.github.com>
2024-03-06 22:41:18 +0600

    05% Done

---
+++ assets/js/custom.js
@@ -0,0 +1,104 @@
+(function($) {
+
+	'use strict';
+
+	/**
+	 	<=={Master JS}==>
+	 	01 Header Sticky JS
+		02 Mobile Menu JS
+		03 Go Top JS
+		04 Preloader JS
+		05 scrollCue JS
+		06 Services Slide JS
+		07 Case Slide JS
+		08 Testimonial Slide JS
+		09 Partner Slide JS
+		10 Partner Slide JS
+		11 Counter JS
+		12 Popup JS
+		13 Password JS
+		14 Curt BTN JS
+		15 Background Image JS
+	**/
+
+	/**<<=== 02 Mobile Menu JS ==>>**/
+	$('.for-mobile-menu').meanmenu({
+		meanScreenWidth: "991",
+	});
+	
+	/**<<=== 01 Header Sticky JS ==>>**/
+	$(window).on('scroll', function() {
+		if ($(this).scrollTop() >150){  
+			$('.navbar').addClass("is-sticky");
+		}
+		else{
+			$('.navbar').removeClass("is-sticky");
+		};
+	});
+	
+	$('.scroll-btn, .navbar .navbar-nav li a').on('click', function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top - 100
+		}, 100);
+		e.preventDefault();
+	});
+	$('.navbar .navbar-nav li a').on('click', function(){
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	/**<<=== 03 Go Top JS ==>>**/
+	$('.back-to-top').on('click', function() {
+		$("html, body").animate({ scrollTop: "0" }, 50);
+	});
+	$(window).on('scroll', function() {
+		var scrolled = $(window).scrollTop();
+		if (scrolled > 300) $('.back-to-top').addClass('active');
+		if (scrolled < 300) $('.back-to-top').removeClass('active');
+	});
+
+	/**<<=== 04 Preloader JS ==>>**/
+	$(window).on('load', function() {
+		$('.preloader').addClass('preloader-deactivate');
+	}) 
+
+	/**<<=== 05 scrollCue JS ==>>**/
+	scrollCue.init();
+
+	/**<<=== 09 Partner Slide JS ==>>**/
+	$('.review-slide').owlCarousel({
+		items: 1,
+		loop: true,
+		margin: 30,
+		nav: true,
+		dots: false,
+		autoplay: true,
+		smartSpeed: 1000,
+		autoplayHoverPause: true,
+		navText: [
+			"<i class='fa-solid fa-chevron-left'></i>",
+			"<i class='fa-solid fa-chevron-right'></i>",
+		],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			576: {
+				items: 2,
+			},
+			768: {
+				items: 2,
+			},
+			992: {
+				items: 2,
+			},
+			1200: {
+				items: 3,
+			},
+			1500: {
+				items: 3,
+			},
+		},
+	});
+
+})(jQuery);
