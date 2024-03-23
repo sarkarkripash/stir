f714cb97e2c3737d6ec2e33f2ff5f5c69c9d11f9
Sarkar Kripash <82843308+sarkarkripash@users.noreply.github.com>
2024-03-24 01:41:12 +0600

    80% Done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -217,9 +217,33 @@
 	});
 
 	/**<<=== 11 Counter JS ==>>**/
-    $('.counter').counterUp({
-        time: 1000
-    });
+    // $('.counter').counterUp({
+    //     time: 1000
+    // });
+
+	// MixItUp JS
+	try {
+        var mixer = mixitup('#Container', {
+            controls: {
+                toggleDefault: 'none'
+            }
+        });
+    } catch (err) {}
+
+
+	const passwordInput = document.getElementById("password");
+	const passwordToggleIcon = document.querySelector(".password-toggle-icon");
+	passwordToggleIcon.addEventListener("click", function() {
+		if (passwordInput.type === "password") {
+		passwordInput.type = "text";
+		passwordToggleIcon.classList.remove("fa-eye-slash");
+		passwordToggleIcon.classList.add("fa-eye");
+		} else {
+		passwordInput.type = "password";
+		passwordToggleIcon.classList.remove("fa-eye");
+		passwordToggleIcon.classList.add("fa-eye-slash");
+		}
+	});
 
 
 })(jQuery);
